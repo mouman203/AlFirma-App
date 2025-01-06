f524e155700f8f72fe2687b86af3bd84a955182c
BENYAHIA Boualem <b_benyahia@outlook.com>
2025-01-06 21:57:05 +0100

    raho ja el jdid

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,13 +5,20 @@
 import FlutterMacOS
 import Foundation
 
-<<<<<<< HEAD
+import cloud_firestore
+import firebase_auth
+import firebase_core
+import firebase_messaging
+import firebase_storage
+import google_sign_in_ios
 import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseMessagingPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseMessagingPlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-=======
-
-func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
->>>>>>> 8744601f3f904c6037dab201a6b10a61d1f91dcb
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,21 +7,12 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-<<<<<<< HEAD
 import 'package:agriplant/main.dart';
-=======
-
-import 'package:startupapp/main.dart';
->>>>>>> 8744601f3f904c6037dab201a6b10a61d1f91dcb
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-<<<<<<< HEAD
     await tester.pumpWidget(const MainApp());
-=======
-    await tester.pumpWidget(const MyApp());
->>>>>>> 8744601f3f904c6037dab201a6b10a61d1f91dcb
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
